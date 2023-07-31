778150aceb2d09347fa569fb21c0a2414dbb5866
ayaKhrnoub <ayaKhrnoub@users.noreply.github.com>
2023-07-31 12:06:22 +0000

    deploy: 333eca94c8cf1e5d83493caa165884a5a2d6a47a

---
+++ assets/Square-c18796a3.js
@@ -0,0 +1,2 @@
+import{j as r}from"./index-b27c7740.js";const e=()=>r.jsx("div",{className:`w-[75px] h-[50px] bg-[#8EA3AA] relative after:absolute\r
+                after:w-[75px] after:z-20 after:h-[50px] after:-top-[50%] after:-right-[50%] after:bg-[#CAD2D3]/70`});export{e as S};
